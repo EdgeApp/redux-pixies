a73b670aed0ade289152a379e213566508ac3672
William Swanson <swansontec@gmail.com>
2017-09-21 21:32:15 -0700

    Remove helpers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 // @flow
 export { Pixie, attachPixie, createPixie, startPixie } from './pixies.js'
 export type { PixieChild, PixieTree } from './pixies.js'
-
-export { InjectContext, ReduxProvider, connectPixie } from './helpers.js'
-export type { MapStateToProps, MapDispatchToProps } from './helpers.js'
