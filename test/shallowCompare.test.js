61eafc98fcedb0d269d4da07fb4699a0b4657af0
William Swanson <swansontec@gmail.com>
2017-09-28 14:16:01 -0700

    Add `shallowCompare` unit test

---
+++ test/shallowCompare.test.js
@@ -0,0 +1,23 @@
+import { shallowCompare } from '../src/managers/util.js'
+import { assert } from 'chai'
+import { describe, it } from 'mocha'
+
+describe('shallow compare', function () {
+  it('compares values', function () {
+    const leaf = {}
+    assert(!shallowCompare(1, 2))
+    assert(!shallowCompare(1, '1'))
+    assert(!shallowCompare(1, null))
+    assert(!shallowCompare({ a: 1 }, {}))
+    assert(!shallowCompare({}, { a: 1 }))
+    assert(!shallowCompare({ a: 1 }, { a: 2 }))
+    assert(!shallowCompare({ a: {} }, { a: {} }))
+    assert(!shallowCompare([1, 2], [1]))
+    assert(!shallowCompare([1, 2], [1, '2']))
+
+    assert(shallowCompare(1, 1))
+    assert(shallowCompare({ a: leaf }, { a: leaf }))
+    assert(shallowCompare([1, 2], [1, 2]))
+    assert(shallowCompare(new Date(20), new Date(20)))
+  })
+})
